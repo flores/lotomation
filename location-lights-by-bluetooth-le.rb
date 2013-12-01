bf38733cd22b2532ee6348acc75a55485a7f37d9
flores <lo@js.la>
2013-11-30 18:35:44 -0800

    make zone devices an array instead of regex

--- location-lights-by-bluetooth-le.rb
+++ location-lights-by-bluetooth-le.rb
@@ -25,13 +25,13 @@ end
 def checkpoint_interpret_rawlocation(rawlocation)
   if rawlocation <= CONFIG['location']['doorcouch']['min']
     "doorcouch"
-  elsif rawlocation < CONFIG['location']['middle']['max'] && 
+  elsif rawlocation < CONFIG['location']['middle']['max'] &&
     rawlocation > CONFIG['location']['middle']['min']
     "middle"
   end
 end
 
-def set_devices(location)
+def get_devices(location)
   CONFIG['location'][location]['devices']
 end
 
@@ -42,26 +42,25 @@ DEVICES = NinjaBlocks::Device.list(:device_type => 'rf433')
 def flip(name)
   DEVICES.each do |device|
     device.sub_devices.each do |subdevice|
-      if subdevice.short_name =~ /#{name}/
+      if subdevice.short_name == name
 	device.actuate(subdevice.data)
+	puts "flipped #{name}"
       end
     end
   end
 end
 
 # Gross and listen inside hcidump
-begin
-  PTY.spawn("hcidump |grep -A3 #{CONFIG['beacon']['fitbit']}")
-  do |stdin, stdout, pid|
-    begin
-      stdin.each do |line|
-	if line =~ /RSSI: (.+?)\n/
-	  rawloc = $1.to_i
-	  location = checkpoint_interpret_rawlocation(rawloc)
-	  if location != checkpoint_get_location
-	    checkpoint_set_location(location)
-	    flip(set_devices(location))
-	  end
+PTY.spawn("hcidump |grep -A3 #{CONFIG['beacon']['fitbit']}")
+do |stdin, stdout, pid|
+  stdin.each do |line|
+    if line =~ /RSSI: (.+?)\n/
+      rawloc = $1.to_i
+      location = checkpoint_interpret_rawlocation(rawloc)
+      if location && location != checkpoint_get_location
+        checkpoint_set_location(location)
+        get_devices(location).each do |device|
+	  flip(device)
 	end
       end
     end
