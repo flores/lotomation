16759f6ee841096e21932fe215c64fac41837cdd
flores <lo@js.la>
2013-12-25 17:11:50 -0800

    have it remember

--- lib/lotomation/power.rb
+++ lib/lotomation/power.rb
@@ -1,6 +1,14 @@
 module Lotomation
   module Power
 
+    def power-current-state(name)
+      File.read("#{Config['status']['dir']}/#{name}")
+    end
+
+    def power-write-state(name, state)
+      File.write("#{Config['status']['dir']}/#{name}", state)
+    end
+
     def flip(name)
       @devices.each do |device|
 	device.sub_devices.each do |subdev|
