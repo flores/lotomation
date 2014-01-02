41508ba90af8cb43e72f3549e8f5a2b050aa3d6e
flores <lo@js.la>
2014-01-02 13:16:57 -0800

    add the bt4 tracker

---
+++ bin/tracker-bt.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# this guy constantly scans bluetooth devices and posts signal strength to the webservice
+
+require 'pty'
+require 'yaml'
+
+hostname="hostname"
+
+config=YAML.load_file('etc/config.yaml')
+server="#{config['webservice']['ip']}:#{config['webservice']['port']}"
+
+hcitoolpid = fork { exec "hcitool lescan" }
+Process.detach(hcitoolpid)
+
+PTY.spawn("hcidump |grep -A4 #{config['devices']['fitbit']}") do |stdin, stdout, pid|
+  stdin.each do |line|
+    exec "curl -d 'rssi=#{$1}' #{server}/tracker/#{hostname}" if line =~ /RSSI: (.+)/
+  end
+end
