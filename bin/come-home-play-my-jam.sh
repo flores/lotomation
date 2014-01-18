81b973a7edd21cfaf03a38bb5f9c773d3be5d2c1
cflores <flores@petalphile.com>
2014-01-17 19:34:04 -0800

    play a random jam from a directory

--- bin/come-home-play-my-jam.sh
+++ bin/come-home-play-my-jam.sh
@@ -6,11 +6,13 @@ host=`awk '/host:/ {print $2}' etc/config.yaml`
 port=`awk '/port:/ {print $2}' etc/config.yaml`
 user=`awk '/user:/ {print $2}' etc/config.yaml`
 pass=`awk '/pass:/ {print $2}' etc/config.yaml`
+jamdir=`awk '/jamdir:/ {print $2}' etc/config.yaml`
 
 lohome=`curl $user:$pass@$host:$port/lo/home`
 jamplayed=`curl $user:$pass@$host:$port/jam/played`
 
 if [[ $lohome == 'yes' ]] && [[ $jamplayed == 'false' ]]; then
-  mplayer -ao alsa:device=hw=1.0 shoorahshoorah.mp3 && curl -d '' $user:$pass@$host:$port/jam/played
+  jam=`ls $jamdir |grep mp3 |shuf |head -1`
+  mplayer -ao alsa:device=hw=1.0 $jamdir/$jam && curl -d '' $user:$pass@$host:$port/jam/played
 fi
 
