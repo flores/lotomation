362edc4d6663de7a9661463aa2f246fe4b188947
cflores <flores@petalphile.com>
2014-03-06 21:32:54 -0800

    jank jam junk

--- lib/lotomation/jams.rb
+++ lib/lotomation/jams.rb
@@ -6,7 +6,19 @@ module Lotomation
     end
 
     def jam_write_state(value)
-      File.write("#{Configs['status']['dir']}/jam", value)
+      File.write("#{Configs['status']['dir']}/jam", value.to_s)
+    end
+    
+    def input_get_state
+      if File.exist?(filename)
+        File.read("#{Configs['status']['dir']}/stereoinput")
+      else
+        File.write("#{Configs['status']['dir']}/jam", 2)
+      end
+    end
+
+    def input_write_state(value)
+      File.write("#{Configs['status']['dir']}/stereoinput", value.to_s)
     end
   
   end
