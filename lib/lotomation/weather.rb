8841fd46f6d8994567a447a5de1f7400ab14850d
cflores <flores@petalphile.com>
2014-05-18 13:09:20 -0700

    add simple storing of wunderground weather data

---
+++ lib/lotomation/weather.rb
@@ -0,0 +1,46 @@
+module Lotomation
+  module Weather
+
+    def weather(city)
+      if city =~ /^(.+), (.+)$/
+        state = $2
+        city = $1.gsub(' ', '_')
+
+        print city
+        print state
+        
+        file = "#{Configs['status']['dir']}/weather-#{city}-#{state}"
+        if File.exist?(file)
+          if (Time.now - File.mtime(file)) > 200
+            print "file is #{Time.now - File.mtime(file)} seconds old"
+            status = weather_update(city, state)
+          else
+            File.read(file)
+          end
+        else
+          weather_update(city, state)
+        end
+      end
+    end
+   
+    def weather_update(city, state)
+      uri = URI.parse("http://api.wunderground.com/api/#{Configs['auth']['wunderground']}/conditions/q/#{state}/#{city}.json")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
+ 
+      response = http.request(request)
+ 
+      if response.code == "200"
+        result = JSON.parse(response.body)
+        
+        weather = result['current_observation']['feelslike_f'] + 'F, ' +
+          result['current_observation']['weather']
+        File.write("#{Configs['status']['dir']}/weather-#{city}-#{state}", weather)
+        weather
+      else
+        "Error"
+      end
+    end
+    
+  end
+end
