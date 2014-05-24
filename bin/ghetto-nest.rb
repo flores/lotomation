d95751a14ed844b743bede2264800e103a5d94c7
cflores <flores@petalphile.com>
2014-05-24 13:02:04 -0700

    heater/ac unit has delays so i think this should too

--- bin/ghetto-nest.rb
+++ bin/ghetto-nest.rb
@@ -8,7 +8,14 @@ config = YAML.load_file('etc/config.yaml')
 auth = "#{config['webserver']['user']}:#{config['webserver']['pass']}"
 server = "#{config['webserver']['host']}:#{config['webserver']['port']}"
 
-state = `curl #{auth}@#{server}/thermostat`
+state = `curl #{auth}@#{server}/thermostat -m 5`
+
+# for my heater/ac unit:
+# pin | purpose (terminal - thermostat wire color)
+#  27 | heating call (W - White) + fan (G - Green), connects to 24V AC (R - Red)
+#  17 | reversing valve (O - Orange), connects to 24V AC specific to airconditioning (Rc - Red)
+#
+# my unit has a delay between turning these on and off and in a certain order, ymmv
 
 if state == "air-conditioner"
   gpio_on = [27, 17]
@@ -28,6 +35,7 @@ if gpio_off.is_a?(Array)
   gpio_off.each do |gpio|
     pin = PiPiper::Pin.new(:pin => gpio.to_i, :direction => :out)
     pin.on
+    sleep 2
   end
 elsif gpio_off
   pin = PiPiper::Pin.new(:pin => gpio_off, :direction => :out)
@@ -35,9 +43,11 @@ elsif gpio_off
 end
 
 if gpio_on.is_a?(Array)
+  # my heater/ac unit has a delay between turning on 
   gpio_on.each do |gpio|
     pin = PiPiper::Pin.new(:pin => gpio.to_i, :direction => :out)
     pin.off
+    sleep 5
   end
 elsif gpio_on
   pin = PiPiper::Pin.new(:pin => gpio_on, :direction => :out)
