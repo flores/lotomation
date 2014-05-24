477610fb2f605e951625627281552dda32e569fb
cflores <flores@petalphile.com>
2014-05-24 03:35:15 -0700

    finally figured out heat on the thermostat

--- bin/ghetto-nest.rb
+++ bin/ghetto-nest.rb
@@ -11,17 +11,12 @@ server = "#{config['webserver']['host']}:#{config['webserver']['port']}"
 state = `curl #{auth}@#{server}/thermostat`
 
 if state == "air-conditioner"
+  gpio_on = [27, 17]
+elsif state == "heater"
   gpio_on = 27
   gpio_off = 17
-elsif state == "heater"
-  gpio_on = 17
-  gpio_off = 27
 elsif state == "off"
-  [17, 27].each do |gpio|
-    pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
-    pin.on
-  end
-  exit
+  gpio_off = [17, 27]
 else
   puts "i do not know this device"
   exit
@@ -29,8 +24,22 @@ end
 
 # first turn off everything (ac and heater cannot both be on)
 # pins are reversed to avoid either turning on at boot/reset
-pin = PiPiper::Pin.new(:pin => gpio_off, :direction => :out)
-pin.on
+if gpio_off.is_a?(Array)
+  gpio_off.each do |gpio|
+    pin = PiPiper::Pin.new(:pin => gpio.to_i, :direction => :out)
+    pin.on
+  end
+elsif gpio_off
+  pin = PiPiper::Pin.new(:pin => gpio_off, :direction => :out)
+  pin.on
+end
 
-pin = PiPiper::Pin.new(:pin => gpio_on, :direction => :out)
-pin.off
+if gpio_on.is_a?(Array)
+  gpio_on.each do |gpio|
+    pin = PiPiper::Pin.new(:pin => gpio.to_i, :direction => :out)
+    pin.off
+  end
+elsif gpio_on
+  pin = PiPiper::Pin.new(:pin => gpio_on, :direction => :out)
+  pin.off
+end
