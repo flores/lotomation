fd476dd8f57877177fa6d37ac9d569ac9ada5226
cflores <flores@petalphile.com>
2014-05-24 01:21:52 -0700

    whoops do not need this

--- lib/lotomation/nightlight.rb
+++ lib/lotomation/nightlight.rb
@@ -14,18 +14,5 @@ module Lotomation
       end        
     end
 
-    def degrees_covert_to_f(rawtemperature)
-      temp_c = rawtemperature.to_f / 1000
-      (temp_c * 9 / 5) + 32
-    end
-
-    def thermostat_write_state(state)
-      File.write("#{Configs['status']['dir']}/thermostat", state)
-    end
-
-    def thermostat_get_state
-      File.read("#{Configs['status']['dir']}/thermostat")
-    end
-
   end
 end
