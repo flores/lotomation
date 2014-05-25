b446bfbfd48eff3a958d56d889807553814950db
cflores <flores@petalphile.com>
2014-05-25 16:36:36 -0700

    save some typing with these helpers

--- lib/lotomation.rb
+++ lib/lotomation.rb
@@ -18,6 +18,9 @@ require 'lotomation/location-by-bluetooth'
 require 'lotomation/jams'
 require 'lotomation/camera'
 require 'lotomation/weather'
+require 'lotomation/traffic'
+require 'lotomation/thermostat'
+require 'lotomation/nightlight'
 
 module Lotomation
   include Power
@@ -26,8 +29,38 @@ module Lotomation
   include Jams
   include Camera
   include Weather
+  include Traffic
+  include Thermostat
+  include Nightlight
 
   def log(message)
     puts message if Configs['status']['verbose']
   end
+
+  def check_state(something)
+    if File.exist?(Configs['status']['dir'] + '/' + something)
+      File.read(Configs['status']['dir'] + '/' + something)
+    else
+      write_state(something, "off")
+      "off"
+    end
+  end
+
+  def write_state(something, state)
+    File.write(Configs['status']['dir'] + '/' + something, state)
+  end
+
+  def check_value(something)
+    if File.exist?(Configs['status']['dir'] + '/' + something)
+      File.read(Configs['status']['dir'] + '/' + something)
+    else
+      write_state(something, "unknown")
+      "unknown"
+    end
+  end
+
+  def write_value(something, value)
+    File.write(Configs['status']['dir'] + '/' + something, value)
+  end 
+
 end
