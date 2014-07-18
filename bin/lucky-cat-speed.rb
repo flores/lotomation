a914367fd26bebedd482398de1c1cb08f9ff3f22
root <root@raspberrypi.(none)>
2014-07-18 21:40:04 +0000

    CRY cycle it for @cortesoft

--- bin/lucky-cat-speed.rb
+++ bin/lucky-cat-speed.rb
@@ -4,26 +4,64 @@ require 'pi_piper'
 include PiPiper
 
 cliarg = ARGV[0]
-pins = [4, 17, 18, 22, 23, 27]
+@pins = [4, 17, 18, 22, 23, 27]
+
+def all_off
+  @pins.each do |gpio|
+    pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+    pin.on
+    puts "turned #{gpio} off"
+  end
+end
 
 if cliarg == "off"
-  pins.each do |gpio|
-    pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
-    pin.off
+  all_off
+elsif cliarg == "cycle"
+  while true do
+    @pins.each do |gpio|
+      pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+      puts "turned #{gpio} on"
+      pin.on
+    end
+    sleep 30
+    all_off
+    sleep 15
+    @pins.each do |gpio|
+      pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+      pin.on
+      puts "turned #{gpio} on"
+      sleep 5
+    end
+    all_off
+    sleep 15
+    @pins.first(3).each do |gpio|
+      pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+      pin.on
+      puts "turned #{gpio} on"
+      sleep 15
+    end
+    all_off
+    sleep 15
+    @pins.drop(3).each do |gpio|
+      pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+      pin.on
+      puts "turned #{gpio} on"
+      sleep 15
+    end
+    all_off
+    sleep 15
   end
-  puts "turned off all pins"
-  exit
 
 elsif cliarg == "on"
 elsif cliarg == "firston"
-  pins = pins.first(3)
+  @pins = @pins.first(3)
 elsif cliarg == "laston"
-  pins = pins.drop(3)
+  @pins = @pins.drop(3)
 else 
-  pins = cliarg.to_i
+  @pins = cliarg.to_i
 end
 
-pins.each do |gpio|
+@pins.each do |gpio|
   pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
   pin.on
   puts "turning on #{gpio}"
