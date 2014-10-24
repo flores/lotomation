eb31d7a790f2fe1db601f352e5ee859970b8c2c9
flores <flores@petalphile.com>
2014-10-24 12:45:56 -0700

    switch to zips instead of citynames for weather

--- lib/lotomation/weather.rb
+++ lib/lotomation/weather.rb
@@ -1,40 +1,30 @@
 module Lotomation
   module Weather
 
-    def weather(city)
-      #if city =~ /^(.+), (.+)$/
-        #state = $2
-        #city = $1.gsub(' ', '_')
-
-        #print city
-        #print state
-
-        #file = "#{Configs['status']['dir']}/weather-#{city}-#{state}"
-        file = "#{Configs['status']['dir']}/weather-#{city}"
-        if File.exist?(file)
-          if (Time.now - File.mtime(file)) > 1800
-            print "file is #{Time.now - File.mtime(file)} seconds old"
-            status = weather_update(city)
-          else
-            File.read(file)
-          end
+    def weather(zip)
+      file = "#{Configs['status']['dir']}/weather-#{zip}"
+      if File.exist?(file)
+        if (Time.now - File.mtime(file)) > 1800
+          print "file is #{Time.now - File.mtime(file)} seconds old"
+          status = weather_update(zip)
         else
-          weather_update(city)
+          File.read(file)
         end
-      #end
+      else
+        weather_update(zip)
+      end
     end
 
-#    def weather_update(city, state)
     def weather_update(zip)
       uri = URI.parse("http://api.wunderground.com/api/#{Configs['auth']['wunderground']}/conditions/q/#{zip}.json")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
 
       response = http.request(request)
- 
+
       if response.code == "200"
         result = JSON.parse(response.body)
-        
+
         weather = result['current_observation']['feelslike_f'] + 'F, ' +
           result['current_observation']['weather']
         File.write("#{Configs['status']['dir']}/weather-#{zip}", weather)
@@ -43,14 +33,14 @@ module Lotomation
         "Error"
       end
     end
-    
+
     def weather_picture_update(zip)
       uri = URI.parse("http://api.wunderground.com/api/#{Configs['auth']['wunderground']}/webcams/q/#{zip}.json")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
- 
+
       response = http.request(request)
- 
+
       if response.code == "200"
         result = JSON.parse(response.body)
         picturearray = Array.new
