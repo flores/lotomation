f84206c5463d0a93e59a3e929c56117581c94e75
flores <flores@petalphile.com>
2014-10-26 13:36:50 -0700

    fix the hvac-checking nightlight

--- bin/hvac-nightlight.rb
+++ bin/hvac-nightlight.rb
@@ -1,37 +1,50 @@
 #!/usr/bin/env ruby
 
+# lo's really goofy HVAC checking nightlight!
+# powers a LED nightlight connected to GPIO on a Raspberry Pi
+# if the HVAC is set to maintain a temperature, gpio pin 17 is on
+# if not, gpio pin 27 is on
+
 require 'yaml'
 require 'pi_piper'
 include PiPiper
 
 config = YAML.load_file('etc/config.yaml')
 auth = "#{config['webserver']['user']}:#{config['webserver']['pass']}"
-server = "#{config['webserver']['host']}:#{config['webserver']['port']}"
+proto = "#{config['webserver']['protocol']}"
+server = "#{config['webserver']['host']}"
+laststatefile = "#{config['status']['dir']}/nightlight_check"
 
-nightlight = `curl  #{auth}@#{server}/nightlight`
+nightlight = `curl -k #{proto}://#{auth}@#{server}/nightlight`
 
 if nightlight == 'on'
-  state = `curl #{auth}@#{server}/hvac`
-  if state == "air-conditioner"
-    gpio_on = 27
-    gpio_off = 17
-  elsif state == "heater"
-    gpio_on = 17
-    gpio_off = 27
-  elsif state == "off"
-    [17, 27].each do |gpio|
-      pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
-      pin.off
+
+  currentstate = `curl -k #{proto}://#{auth}@#{server}/maintain/enforce`
+  laststate = File.exist?(laststatefile)? File.read(laststatefile) : "off"
+
+  if currentstate != laststate
+    if maintain == "on"
+      gpio_on = 17
+      gpio_off = 27
+    else
+      gpio_on = 27
+      gpio_off = 17
     end
-    exit
-  else
-    puts "i do not know this device"
-    exit
+
+    pin = PiPiper::Pin.new(:pin => gpio_off, :direction => :out)
+    pin.off
+
+    pin = PiPiper::Pin.new(:pin => gpio_on, :direction => :out)
+    pin.on
+
+    File.write(laststatefile, currentstate)
   end
-  
-  pin = PiPiper::Pin.new(:pin => gpio_off, :direction => :out)
-  pin.off
-  
-  pin = PiPiper::Pin.new(:pin => gpio_on, :direction => :out)
-  pin.on
+
+else
+
+  [17, 27].each do |gpio|
+    pin = PiPiper::Pin.new(:pin => gpio, :direction => :out)
+    pin.off
+  end
+
 end
